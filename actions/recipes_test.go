1792715f00a08a43fb63720a079615c48c1cb75a
SerhiiCho <serhiicho@protonmail.com>
2019-09-29 21:58:12 +0300

    Added empty test for recipe model

---
+++ actions/recipes_test.go
@@ -0,0 +1,36 @@
+package actions
+
+import (
+  "testing"
+
+  "github.com/stretchr/testify/require"
+)
+
+func (as *ActionSuite) Test_RecipesResource_List() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_Show() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_Create() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_Update() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_Destroy() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_New() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_RecipesResource_Edit() {
+  as.Fail("Not Implemented!")
+}
+
