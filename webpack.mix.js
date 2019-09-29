20b4dd77037a3a8c1180200cf82584f5f231b5c9
SerhiiCho <serhiicho@protonmail.com>
2019-09-29 22:42:50 +0300

    Renamed assets dir to resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,13 +5,13 @@ const js = 1
 const chartjs = 0
 
 if (css == 1) {
-    mix.sass('assets/css/app.sass', 'public/assets/app.css').options({
+    mix.sass('resources/sass/app.sass', 'public/assets/app.css').options({
         processCssUrls: false,
     })
 }
 
 if (js == 1) {
-    mix.js('assets/js/app.js', 'public/assets/app.js')
+    mix.js('resources/js/app.js', 'public/assets/app.js')
         .options({
             uglify: {
                 uglifyOptions: {
@@ -25,7 +25,7 @@ if (js == 1) {
 }
 
 if (chartjs == 1) {
-    mix.js('assets/js/vue/chart/chart.js', 'public/assets/chart.js')
+    mix.js('resources/js/vue/chart/chart.js', 'public/assets/chart.js')
         .options({
             uglify: {
                 uglifyOptions: {
