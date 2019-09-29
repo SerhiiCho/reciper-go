7887ff8fc3cde6627a6eec9d4fd94249ae8548c5
SerhiiCho <serhiicho@protonmail.com>
2019-09-29 21:57:58 +0300

    Added recipe.go model

---
+++ models/recipe.go
@@ -0,0 +1,12 @@
+package models
+
+// Recipe model
+type Recipe struct {
+	ID      uint   `json:"id,omitempty"`
+	TitleRu string `json:"title_ru,omitempty"`
+	TitleEn string `json:"title_en,omitempty"`
+	IntroRu string `json:"intro_ru,omitempty"`
+	IntroEn string `json:"intro_en,omitempty"`
+	TextRu  string `json:"text_ru,omitempty"`
+	TextEn  string `json:"text_en,omitempty"`
+}
