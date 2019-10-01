774330b0c2a2cf26ffc1341a3293c99e9ecc4bb9
SerhiiCho <serhiicho@protonmail.com>
2019-10-01 18:41:52 +0300

    Added vue js

--- resources/js/vue/vue.js
+++ resources/js/vue/vue.js
@@ -1,6 +1,12 @@
 import './settings/bootstrap'
 import './components'
+import App from './App'
+import router from './router'
+import store from './store'
 
 new Vue({
+    router,
+    store,
+    render: h => h(App),
     el: '#app',
 })
