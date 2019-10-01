e2997dac44a365fae6f7113128d8b19568720cb3
SerhiiCho <serhiicho@protonmail.com>
2019-10-01 22:10:58 +0300

    Added i18n package

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,6 @@
 <template>
     <div id="app">
         <Navbar />
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
         <router-view/>
     </div>
 </template>
--- app/src/main.js
+++ app/src/main.js
@@ -4,13 +4,25 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import axios from 'axios'
+import VueI18n from 'vue-i18n'
+import ru from './locales/ru.json'
+import en from './locales/en.json'
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 window.Event = new Vue()
 
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: { en, ru },
+})
+
 new Vue({
     router,
     store,
+    i18n,
     render: h => h(App)
 }).$mount('#app')
