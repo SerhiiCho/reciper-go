b983f734221d6b744f6b73e81091669d4f160f76
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 00:38:04 +0300

    Refactored main.go, added init function

--- src/main.go
+++ src/main.go
@@ -1,51 +1,23 @@
 package main
 
 import (
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
+	"os"
+
+	"github.com/SerhiiCho/reciper_go/src/bootstrap"
+	"github.com/joho/godotenv"
 )
 
-// MyHandler struct
-type MyHandler struct{}
-
-func (handler *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	path := "app/dist/" + string(r.URL.Path[1:])
-
-	log.Println(path)
-
-	if path == "app/dist/" {
-		path = "app/dist/index.html"
-	}
-
-	data, err := ioutil.ReadFile(path)
+func init() {
+	err := godotenv.Load()
 
 	if err != nil {
-		w.WriteHeader(404)
-		w.Write([]byte("404 My friend"))
-		return
+		log.Fatal("Error loading .env file")
 	}
-
-	var contentType string
-
-	if strings.HasSuffix(path, ".css") {
-		contentType = "text/css"
-	} else if strings.HasSuffix(path, ".png") {
-		contentType = "image/png"
-	} else if strings.HasSuffix(path, ".js") {
-		contentType = "application/javascript"
-	} else if strings.HasSuffix(path, ".svg") {
-		contentType = "image/svg+xml"
-	} else {
-		contentType = "text/html"
-	}
-
-	w.Header().Add("Content-Type", contentType)
-	w.Write(data)
 }
 
 func main() {
-	http.Handle("/", new(MyHandler))
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", bootstrap.LoadStaticFiles)
+	http.ListenAndServe(os.Getenv("APP_PORT"), nil)
 }
