30335e71cb35f217079de518d16a0494d4664632
SerhiiCho <serhiicho@protonmail.com>
2019-10-01 20:30:24 +0300

    Added Header.vue

---
+++ app/src/includes/home/Header.vue
@@ -0,0 +1,64 @@
+<template>
+    <header class="home-header" :style="{'background-image': `url('${bgImageURL}')`}">
+        <div class="home-header__overlay"></div>
+        <div class="home-header__content">
+            <h1 class="home-header__content__title">@lang('messages.app_name')</h1>
+            <div class="home-header__content__meal">
+                <div>
+                    <a href="/recipes#simple" class="home-header__content__meal__word">
+                        @lang('home.show_simple_recipes')
+                    </a>
+                </div>
+                <div>
+                    <a href="/recipes#breakfast" class="home-header__content__meal__word">
+                        @lang('home.breakfast')
+                    </a>, 
+                    <a href="/recipes#lunch" class="home-header__content__meal__word">
+                        @lang('home.lunch')
+                    </a>
+                    @lang('home.or') 
+                    <a href="/recipes#dinner" class="home-header__content__meal__word">
+                        @lang('home.dinner')
+                    </a>
+                </div>
+                <a class="modal-trigger main-dark px-3 home-header__content__meal__word"
+                    href="#add-recipe-modal"
+                >
+                    @lang('home.add_your_recipe')
+                </a>
+            </div> <!-- / .home-header__content__meal-wrap -->
+
+            <!--  Search Form  -->
+            <form action="/search" method="get">
+                <div class="position-relative mt-4">
+                    <div class="home-header__content__search-wrap" id="home-search-form">
+                        <input type="search"
+                            name="for"
+                            id="header-search-input"
+                            placeholder="@lang('pages.search_details')"
+                            class="home-header__content__search-wrap__input"
+                        />
+                    </div> <!-- / .home-header__content__search-wrap -->
+
+                    <!-- Search button -->
+                    <button type="submit"
+                        id="home-search-btn"
+                        class="home-header__content__btn"
+                    >
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bgImageURL: require('@/assets/img/other/header/by-daria-yakovleva-3.jpg'),
+        }
+    }
+}
+</script>
