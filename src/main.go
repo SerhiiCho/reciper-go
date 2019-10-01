f38c520860d0e6fba7f2f55948198acaa45caeca
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 00:14:30 +0300

    Added functionality for serving index.html

--- src/main.go
+++ src/main.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
-
-	"github.com/SerhiiCho/reciper_go/src/controllers"
+	"strings"
 )
 
+// MyHandler struct
+type MyHandler struct{}
+
+func (handler *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := "app/dist/" + string(r.URL.Path[1:])
+
+	log.Println(path)
+
+	if path == "app/dist/" {
+		path = "app/dist/index.html"
+	}
+
+	data, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte("404 My friend"))
+		return
+	}
+
+	var contentType string
+
+	if strings.HasSuffix(path, ".css") {
+		contentType = "text/css"
+	} else if strings.HasSuffix(path, ".png") {
+		contentType = "image/png"
+	} else if strings.HasSuffix(path, ".js") {
+		contentType = "application/javascript"
+	} else if strings.HasSuffix(path, ".svg") {
+		contentType = "image/svg+xml"
+	} else {
+		contentType = "text/html"
+	}
+
+	w.Header().Add("Content-Type", contentType)
+	w.Write(data)
+}
+
 func main() {
-	http.HandleFunc("/", controllers.HomeController)
+	http.Handle("/", new(MyHandler))
 	http.ListenAndServe(":3000", nil)
 }
