353c14d5cc0428551e58761511e48408559be509
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 12:06:50 +0300

    Changed Footer.vue

--- app/src/includes/Footer.vue
+++ app/src/includes/Footer.vue
@@ -36,50 +36,50 @@
             <div class="col s12 m6 l3 text-left row">
                 <ul class="px-0 unstyled-list col s12">
                     <li><strong>{{ $t('pages.navigation') }}</strong></li>
+
                     <!-- Home page -->
-                    <li>
-                        <a href="/" :title="$t('messages.go')">
+                    <router-link to="/" tag="li" class="brand-logo no-select">
+                        <a>
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i> 
                             {{ $t('home.home') }}
                         </a>
-                    </li>
+                    </router-link>
+
                     <!-- Add recipe -->
                     <li>
-                        <a href="/recipes#new" :title="$t('messages.go')">
+                        <a href="/recipes#new">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i> 
                             {{ $t('recipes.recipes') }}
                         </a>
                     </li>
+
                     <!-- Search -->
                     <li>
-                        <a href="/search" :title="$t('messages.go')">
+                        <a href="/search">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('pages.search') }}
                         </a>
                     </li>
+
                     <!-- Help -->
                     <li>
-                        <a href="docs/help"
-                            :title="$t('messages.go')"
-                            target="_blank"
-                        >
+                        <a href="docs/help" target="_blank">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('messages.help') }}
                         </a>
                     </li>
+
                     <!-- Contact -->
                     <li>
-                        <a href="/contact" :title="$t('messages.go')">
+                        <a href="/contact">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('feedback.contact_us') }}
                         </a>
                     </li>
+
                     <!-- Documents -->
                     <li>
-                        <a href="/docs"
-                            :title="$t('messages.go')"
-                            target="_blank"
-                        >
+                        <a href="/docs" target="_blank">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('documents.documents') }}
                         </a>
@@ -87,14 +87,15 @@
 
                     <!-- Login -->
                     <li>
-                        <a href="/login" :title="$t('messages.go')">
+                        <a href="/login">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('auth.login') }}
                         </a>
                     </li>
+
                     <!-- Register -->
                     <li>
-                        <a href="/register" :title="$t('messages.go')">
+                        <a href="/register">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i>
                             {{ $t('auth.register') }}
                         </a>
@@ -108,6 +109,7 @@
                             <strong>{{ $t('documents.info') }}</strong>
                         </a>
                     </li>
+
                     <li>
                         <a href="/docs/terms" target="_blank">
                             <i class="fas fa-angle-right red-text" style="width:7.5px"></i> 
@@ -121,17 +123,13 @@
         <!-- Help & Contact us buttons -->
         <div class="center pt-2">
             <div>
-                <a href="/contact"
-                    :title="$t('messages.go')"
-                    class="mr-3"
-                >
+                <a href="/contact" class="mr-3">
                     <i class="fas fa-envelope mr-1" style="width:15px"></i>
                     {{ $t('feedback.contact_us') }}
                 </a>
-                <a href="/docs/help"
-                    :title="$t('messages.go')"
-                    target="_blank"
-                >
+
+                
+                <a href="/docs/help" target="_blank">
                     <i class="fas fa-question mr-1" style="width:11px"></i>
                     {{ $t('messages.help') }}
                 </a>
@@ -142,9 +140,10 @@
                 <div class="col s12 m1 l1 offset-m3 offset-l4 m">
                     <img src="@/assets/img/other/logo.png" alt="" height="50">
                 </div>
+
                 <div class="col s12 m5 l3 text-left">
                     <div class="valign-wrapper" style="height:70px">
-                        <span>{{ $t('pages.title_footer') }}<br> &copy; 2019 Reciper</span>
+                        <span>{{ $t('pages.title_footer') }}<br> &copy; {{ currentYear }} {{ $t('messages.app_name') }}</span>
                     </div>
                 </div>
             </div>
@@ -156,7 +155,7 @@
 export default {
     data() {
         return {
-            //
+            currentYear: new Date().getFullYear()
         }
     },
 }
