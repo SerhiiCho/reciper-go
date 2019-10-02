2cbb514bafe740ad2a9838c8bfd87fd4eba2d20d
SerhiiCho <serhiicho@protonmail.com>
2019-10-02 10:49:33 +0300

    Cleaned main.go

--- src/main.go
+++ src/main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/SerhiiCho/reciper_go/src/bootstrap"
-	"github.com/joho/godotenv"
-)
-
-func init() {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-}
+import "github.com/SerhiiCho/reciper_go/src/bootstrap"
 
 func main() {
-	http.HandleFunc("/", bootstrap.LoadStaticFiles)
-	http.ListenAndServe(os.Getenv("APP_PORT"), nil)
+	bootstrap.IndexPage()
 }
