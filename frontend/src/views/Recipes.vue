d9f900e347c034afc3263964687966316a6d3faf
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 18:03:25 +0300

    Formatted Recipes.vue file

--- frontend/src/views/Recipes.vue
+++ frontend/src/views/Recipes.vue
@@ -30,9 +30,9 @@
                             <!-- Add to Favorites button -->
                             <div class="d-inline-block">
                                 <BtnFavs
-                                        :recipe-id="recipe.id"
-                                        :recipe-slug="recipe.slug"
-                                        :items="returnFavs(recipe.id)"
+                                    :recipe-id="recipe.id"
+                                    :recipe-slug="recipe.slug"
+                                    :items="returnFavs(recipe.id)"
                                 ></BtnFavs>
                             </div>
 
@@ -52,116 +52,116 @@
 </template>
 
 <script>
-    import BtnFavs from '@/components/BtnFavs.vue'
-    import Preloader from '@/components/Preloader.vue'
-    import SortButtons from '@/components/SortButtons.vue'
+import BtnFavs from '@/components/BtnFavs.vue'
+import Preloader from '@/components/Preloader.vue'
+import SortButtons from '@/components/SortButtons.vue'
 
-    export default {
-        data() {
-            return {
-                recipes: [],
-                loading: false,
-                url: null,
-                theEnd: false,
-                favs: [],
-            }
-        },
-
-        created() {
-            this.fetchRecipes(true)
-
-            // Call onScroll method if the there are more recipes
-            // window.addEventListener('scroll', () => {
-            //     if (!this.theEnd) this.onScroll()
-            // })
-
-            // Fetch recipes when the hash has changed
-            window.onhashchange = () => {
-                this.theEnd = false
-                this.url = null
-                this.fetchRecipes(true)
-            }
-        },
-
-        methods: {
-            /**
-             * Make request to the server, get recipes json object and
-             * set or concatenate {this.recipes} variable to the response data.
-             * Emit the event hash-changed.
-             *
-             * @param {boolean} reload
-             * @return {void}
-             */
-            fetchRecipes(reload = false) {
-                this.loading = true
-
-                const hash = window.location.hash.substring(1)
-                // const url = this.url === null ? `http://localhost:8080/api/recipes/${hash}` : this.url
-                const url = this.url === null ? `http://localhost:8080/api/recipes` : this.url
-
-                Event.$emit('hash-changed', hash)
-
-                this.$axios.get(url)
-                    .then(res => {
-                        /**
-                         * If pagination has the 'next' value not null, set url variable
-                         * to that 'next' value. Otherwise set theEnd to true.
-                         */
-                        // if (res.data.links.next !== null) {
-                        //     this.url = res.data.links.next
-                        // } else {
-                        //     this.theEnd = true
-                        // }
-
-                        /**
-                         * Set recipes variable to received response data, but if 'reload' variable
-                         * is true concatenate recipes with received response data
-                         */
-                        // this.recipes = reload ? res.data.data : this.recipes.concat(res.data.data)
-                        this.recipes = reload ? res.data : this.recipes.concat(res.data)
-                        this.loading = false
-                        console.log(this.recipes)
-                    })
-                    .catch(err => {
-                        console.error(err)
-                        this.loading = false
-                    })
-
-                this.loading = false
-            },
-
-            /**
-             * Fetch recipes if user scrolled the screen to the bottom
-             * of the browser window.
-             *
-             * @return {void}
-             */
-            onScroll() {
-                const wrap = document.getElementById('recipes-page')
-                const contentHeight = wrap.offsetHeight
-                const yOffset = window.pageYOffset
-                const currentPosition = yOffset + window.innerHeight
-
-                if (currentPosition >= contentHeight && !this.loading) {
-                    this.fetchRecipes()
-                }
-            },
-
-            /**
-             * Return array of favs only with this recipe id
-             *
-             * @param {number} recipeId
-             * @return {object}
-             */
-            returnFavs(recipeId) {
-                return this.favs.filter(fav => fav.recipe_id == recipeId)
-            },
-        },
-
-        components: {
-            Preloader,
-            BtnFavs,
-            SortButtons,
+export default {
+    data() {
+        return {
+            recipes: [],
+            loading: false,
+            url: null,
+            theEnd: false,
+            favs: [],
         }
+    },
+
+    created() {
+        this.fetchRecipes(true)
+
+        // Call onScroll method if the there are more recipes
+        // window.addEventListener('scroll', () => {
+        //     if (!this.theEnd) this.onScroll()
+        // })
+
+        // Fetch recipes when the hash has changed
+        window.onhashchange = () => {
+            this.theEnd = false
+            this.url = null
+            this.fetchRecipes(true)
+        }
+    },
+
+    methods: {
+        /**
+         * Make request to the server, get recipes json object and
+         * set or concatenate {this.recipes} variable to the response data.
+         * Emit the event hash-changed.
+         *
+         * @param {boolean} reload
+         * @return {void}
+         */
+        fetchRecipes(reload = false) {
+            this.loading = true
+
+            const hash = window.location.hash.substring(1)
+            // const url = this.url === null ? `http://localhost:8080/api/recipes/${hash}` : this.url
+            const url = this.url === null ? `http://localhost:8080/api/recipes` : this.url
+
+            Event.$emit('hash-changed', hash)
+
+            this.$axios.get(url)
+                .then(res => {
+                    /**
+                     * If pagination has the 'next' value not null, set url variable
+                     * to that 'next' value. Otherwise set theEnd to true.
+                     */
+                    // if (res.data.links.next !== null) {
+                    //     this.url = res.data.links.next
+                    // } else {
+                    //     this.theEnd = true
+                    // }
+
+                    /**
+                     * Set recipes variable to received response data, but if 'reload' variable
+                     * is true concatenate recipes with received response data
+                     */
+                    // this.recipes = reload ? res.data.data : this.recipes.concat(res.data.data)
+                    this.recipes = reload ? res.data : this.recipes.concat(res.data)
+                    this.loading = false
+                    console.log(this.recipes)
+                })
+                .catch(err => {
+                    console.error(err)
+                    this.loading = false
+                })
+
+            this.loading = false
+        },
+
+        /**
+         * Fetch recipes if user scrolled the screen to the bottom
+         * of the browser window.
+         *
+         * @return {void}
+         */
+        onScroll() {
+            const wrap = document.getElementById('recipes-page')
+            const contentHeight = wrap.offsetHeight
+            const yOffset = window.pageYOffset
+            const currentPosition = yOffset + window.innerHeight
+
+            if (currentPosition >= contentHeight && !this.loading) {
+                this.fetchRecipes()
+            }
+        },
+
+        /**
+         * Return array of favs only with this recipe id
+         *
+         * @param {number} recipeId
+         * @return {object}
+         */
+        returnFavs(recipeId) {
+            return this.favs.filter(fav => fav.recipe_id == recipeId)
+        },
+    },
+
+    components: {
+        Preloader,
+        BtnFavs,
+        SortButtons,
     }
+}
 </script>
