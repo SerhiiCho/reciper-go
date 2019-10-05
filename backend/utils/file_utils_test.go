14914e49b51151a836625f3a2d436780abcadd8d
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 23:39:25 +0300

    Added test for FilePutContent function

--- backend/utils/file_utils_test.go
+++ backend/utils/file_utils_test.go
@@ -49,3 +49,27 @@ func TestFileGetContents(t *testing.T) {
 		}
 	})
 }
+
+func TestFilePutContent(t *testing.T) {
+	t.Parallel()
+
+	t.Run("creates file if it doesn't exist", func(t *testing.T) {
+		RemoveFileIfExist("test-file")
+
+		FilePutContent("test-file", "")
+
+		if !FileExists("test-file") {
+			t.Error("test-file doesn't exist after creation")
+		}
+	})
+
+	t.Run("inserts text in a file", func(t *testing.T) {
+		FilePutContent("test-file", "Hello planet!")
+
+		if FileGetContent("test-file") != "Hello planet!" {
+			t.Error("test-file doesn't contain text `Hello planet!` after inserting it")
+		}
+
+		RemoveFileIfExist("test-file")
+	})
+}
