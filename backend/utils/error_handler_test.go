2b1bcb4a743d386d29db8fa306c5a74b8858d7a2
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 22:09:49 +0300

    Added tests to error_handler_test

--- backend/utils/error_handler_test.go
+++ backend/utils/error_handler_test.go
@@ -10,48 +10,45 @@ import (
 )
 
 func TestHandleError(t *testing.T) {
-	//
+	t.Error("empty test")
 }
 
 func TestCreateLogFileIfDoesntExist(t *testing.T) {
-	//
+	t.Error("empty test")
 }
 
-func TestPrintIfExist(t *testing.T) {
-	t.Run("if argument is nil", func(t *testing.T) {
-		var buf bytes.Buffer
-		log.SetOutput(&buf)
+func TestPrintIfExist_IfArgumentIsNil(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
 
-		defer func() {
-			log.SetOutput(os.Stderr)
-		}()
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
 
-		printIfExist(nil)
+	printIfExist(nil)
 
-		result := buf.String()
+	result := buf.String()
 
-		if result != "" {
-			t.Errorf("Result must be empty string but got %v", result)
-		}
-	})
+	if result != "" {
+		t.Errorf("Result must be empty string but got %v", result)
+	}
+}
 
-	t.Run("if argument is error", func(t *testing.T) {
-		var buf bytes.Buffer
+func TestPrintIfExist_IfArgumentIsError(t *testing.T) {
+	var buf bytes.Buffer
+	log.SetOutput(&buf)
 
-		log.SetOutput(&buf)
+	defer func() {
+		log.SetOutput(os.Stderr)
+	}()
 
-		defer func() {
-			log.SetOutput(os.Stderr)
-		}()
+	printIfExist(errors.New("some error message"))
 
-		printIfExist(errors.New("some error message"))
+	result := buf.String()
+	dateTime := time.Now().Format("2006/01/02 15:04:05")
+	expected := dateTime + " some error message\n"
 
-		result := buf.String()
-		dateTime := time.Now().Format("2006/01/02 15:04:05")
-		expected := dateTime + " some error message\n"
-
-		if result != expected {
-			t.Errorf("Result must be `%s` but got `%s`", expected, result)
-		}
-	})
+	if result != expected {
+		t.Errorf("Result must be `%s` but got `%s`", expected, result)
+	}
 }
