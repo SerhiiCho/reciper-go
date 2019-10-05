59c156f99bbc03bca9e6bc13c2f4bbe2c95c3867
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 22:37:12 +0300

    Removed method from error_handler.go

--- backend/utils/error_handler.go
+++ backend/utils/error_handler.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 	"runtime"
@@ -22,26 +21,21 @@ func HandleError(text string, err error) bool {
 	dateInfo := "[" + date.Format("02-01-2006 15:04:05") + "] "
 	msgError := fileName + ":" + strconv.Itoa(lineNum) + ": " + text + ". " + err.Error() + "\n"
 
-	createLogFileIfDoesntExist("logs.log")
-	logFile, err := os.OpenFile("logs.log", os.O_APPEND|os.O_WRONLY, 0600)
+	if !FileExists("logs.log") {
+		FilePutContent("logs.log", dateInfo+msgError)
+		return true
+	}
 
+	logFile, err := os.OpenFile("logs.log", os.O_APPEND|os.O_WRONLY, 0600)
 	printIfExist(err)
 
 	log.Print(msgError)
 
 	_, writeErr := logFile.WriteString(dateInfo + msgError)
 	printIfExist(writeErr)
-
 	printIfExist(logFile.Close())
 
 	return true
-}
-
-func createLogFileIfDoesntExist(filePath string) {
-	if !FileExists(filePath) {
-		err := ioutil.WriteFile(filePath, []byte(""), 0600)
-		HandleError("Can't create empty log file", err)
-	}
 }
 
 func printIfExist(err error) {
