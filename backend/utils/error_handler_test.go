5f1d6adacb6b90dbac03fc005ee6fa7d6b0b97bf
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 21:29:59 +0300

    Added test to error_handler_test.go

---
+++ backend/utils/error_handler_test.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"bytes"
+	"errors"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestHandleError(t *testing.T) {
+	//
+}
+
+func TestCreateLogFileIfDoesntExist(t *testing.T) {
+	//
+}
+
+func TestPrintIfExist(t *testing.T) {
+	t.Run("if argument is nil", func(t *testing.T) {
+		var buf bytes.Buffer
+		log.SetOutput(&buf)
+
+		defer func() {
+			log.SetOutput(os.Stderr)
+		}()
+
+		printIfExist(nil)
+
+		result := buf.String()
+
+		if result != "" {
+			t.Errorf("Result must be empty string but got %v", result)
+		}
+	})
+
+	t.Run("if argument is error", func(t *testing.T) {
+		var buf bytes.Buffer
+
+		log.SetOutput(&buf)
+
+		defer func() {
+			log.SetOutput(os.Stderr)
+		}()
+
+		printIfExist(errors.New("some error message"))
+
+		result := buf.String()
+		dateTime := time.Now().Format("2006/01/02 15:04:05")
+		expected := dateTime + " some error message\n"
+
+		if result != expected {
+			t.Errorf("Result must be `%s` but got `%s`", expected, result)
+		}
+	})
+}
