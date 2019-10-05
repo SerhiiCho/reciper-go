30f385bd7f1e726889fcb016b59e2eb6cc5b5694
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 22:39:59 +0300

    Changed error message in recipe_test.go file

--- backend/models/recipe_test.go
+++ backend/models/recipe_test.go
@@ -4,11 +4,12 @@ import "testing"
 
 func TestCreateRecipe(t *testing.T) {
 	t.Parallel()
+
 	recipe := NewRecipe()
 	recipe.CreateRecipe(Recipe{})
 
 	if len(recipe.Recipes) != 1 {
-		t.Error("Item was not added")
+		t.Error("Recipe item was not created via CreateRecipe method")
 	}
 }
 
