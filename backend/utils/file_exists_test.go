21a7ae4450328d60d2ceeae659556ce529ce1546
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 21:06:25 +0300

    Added file_exists_test.go

---
+++ backend/utils/file_exists_test.go
@@ -0,0 +1,17 @@
+package utils
+
+import "testing"
+
+func TestFileExists(t *testing.T) {
+	if !FileExists("file_exists_test.go") {
+		t.Error("FileExists must return true because file_exists_test.go file exists")
+	}
+
+	if FileExists("sadlfsdl") {
+		t.Error("FileExists must return false because sadlfsdl file doen't exist")
+	}
+
+	if FileExists("") {
+		t.Error("FileExists must return false because empty string is passed as argument")
+	}
+}
