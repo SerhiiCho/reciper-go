03efdedf6004035c74228137bea44eea4f6878bb
SerhiiCho <serhiicho@protonmail.com>
2019-10-05 23:23:07 +0300

    Added tests for file_utils

--- backend/utils/file_utils.go
+++ backend/utils/file_utils.go
@@ -15,16 +15,28 @@ func FileExists(name string) bool {
 	return true
 }
 
-// FilePutContent creates file and inserts text into it
-func FilePutContent(filePath string, text string) {
-	err := ioutil.WriteFile(filePath, []byte(text), 0600)
-	HandleError("Can't create file in path "+filePath, err)
-}
-
 // RemoveFileIfExist deletes file if it exist
 func RemoveFileIfExist(fileName string) {
 	if FileExists(fileName) {
 		err := os.Remove(fileName)
 		HandleError("Error while trying to delete file", err)
 	}
+}
+
+// FilePutContent creates file and inserts text into it
+func FilePutContent(filePath string, text string) {
+	err := ioutil.WriteFile(filePath, []byte(text), 0600)
+	HandleError("Can't create file in path "+filePath, err)
+}
+
+// FileGetContent returns the content of the given file
+func FileGetContent(filePath string) string {
+	if !FileExists(filePath) {
+		return ""
+	}
+
+	fileText, err := ioutil.ReadFile(filePath)
+	HandleError("File reading error", err)
+
+	return string(fileText)
 }
--- backend/utils/file_utils_test.go
+++ backend/utils/file_utils_test.go
@@ -17,3 +17,35 @@ func TestFileExists(t *testing.T) {
 		t.Error("FileExists must return false because empty string is passed as argument")
 	}
 }
+
+func TestFileGetContents(t *testing.T) {
+	t.Parallel()
+
+	t.Run("returns file content", func(t *testing.T) {
+		FilePutContent("test-file", "Hello world")
+		result := FileGetContent("test-file")
+
+		if result != "Hello world" {
+			t.Errorf("The result must be `Hello world`%v", result)
+		}
+		RemoveFileIfExist("test-file")
+	})
+
+	t.Run("returns empty string if file is empty", func(t *testing.T) {
+		FilePutContent("test-file", "")
+		result := FileGetContent("")
+
+		if result != "" {
+			t.Error("The value of result variable must be empty string")
+		}
+		RemoveFileIfExist("test-file")
+	})
+
+	t.Run("returns empty string if does not exist", func(t *testing.T) {
+		result := FileGetContent("nostradamus")
+
+		if result != "" {
+			t.Error("FileGetContents must return empty string because file doesn't exist")
+		}
+	})
+}
