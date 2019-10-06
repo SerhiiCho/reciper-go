5bbcb77f4398068c4cccbdb4a97d75fc1356e2df
SerhiiCho <serhiicho@protonmail.com>
2019-10-06 16:25:18 +0300

    Added getDB method to main.go fiel

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/SerhiiCho/reciper/backend/http/handler"
 	"github.com/SerhiiCho/reciper/backend/http/middleware"
 	"github.com/SerhiiCho/reciper/backend/models"
 	"github.com/SerhiiCho/reciper/backend/utils"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"os"
 )
 
 func init() {
@@ -18,8 +21,7 @@ func init() {
 func main() {
 	r := gin.Default()
 	recipeRepo := models.NewRecipe()
-	db, err := sql.Open("mysql", "root@tcp(reciper_db:1001)/reciper?charset=utf8")
-	utils.HandleError("Database connection error", err)
+	db := getDB()
 
 	r.Use(middleware.App())
 
@@ -27,4 +29,19 @@ func main() {
 	r.POST("/api/recipes", handler.RecipesCreate(recipeRepo, db))
 
 	utils.HandleError("Can't serve the app", r.Run())
+}
+
+func getDB() *sql.DB {
+	user := os.Getenv("DB_USER")
+	pwd := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8", user, pwd, host, port, name)
+
+	db, err := sql.Open("mysql", dataSource)
+	utils.HandleError("Database connection error", err)
+
+	return db
 }
