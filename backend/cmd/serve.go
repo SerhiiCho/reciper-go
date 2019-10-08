1c745583117dabd3eb27b355a12f71e56877821e
SerhiiCho <serhiicho@protonmail.com>
2019-10-08 17:27:59 +0300

    Cleaned serve.go file

--- backend/cmd/serve.go
+++ backend/cmd/serve.go
@@ -1,36 +1,34 @@
 package cmd
 
 import (
-	"github.com/SerhiiCho/reciper/backend/api"
-	"github.com/SerhiiCho/reciper/backend/app"
+	apiPackage "github.com/SerhiiCho/reciper/backend/api"
+	appPackage "github.com/SerhiiCho/reciper/backend/app"
 	"github.com/SerhiiCho/reciper/backend/app/middleware"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/cobra"
 )
 
-var serveCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "serves the api",
-	Run: func(cmd *cobra.Command, args []string) {
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "serve",
+		Short: "serves the api",
+		Run: func(cmd *cobra.Command, args []string) {
 
-		app := app.NewApp()
-		api := api.NewAPI(app)
+			app := appPackage.NewApp()
+			api := apiPackage.NewAPI(app)
 
-		defer app.Close()
+			defer app.Close()
 
-		router := gin.Default().SecureJsonPrefix("/api")
-		router.Use(getMiddlewares()...)
+			router := gin.Default().SecureJsonPrefix("/api")
+			router.Use(getMiddlewares()...)
 
-		api.Init(router)
-	},
+			api.Init(router)
+		},
+	})
 }
 
 func getMiddlewares() []gin.HandlerFunc {
 	return []gin.HandlerFunc{
 		middleware.AppMiddle(),
 	}
-}
-
-func init() {
-	rootCmd.AddCommand(serveCmd)
 }
