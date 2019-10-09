a3ccf1679b88f96d7b765d0c10d992f15499fec7
SerhiiCho <serhiicho@protonmail.com>
2019-10-09 12:34:04 +0300

    Fixed mistake in model.go

--- backend/model/model.go
+++ backend/model/model.go
@@ -4,5 +4,5 @@ type Model struct {
 	ID        uint   `json:"id" gorm:"type:int(10) unsigned auto_increment;primary_key"`
 	CreatedAt string `json:"created_at" gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
 	UpdatedAt string `json:"updated_at" gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
-	DeletedAt string `json:"updated_at" gorm:"type:datetime;default:null"`
+	DeletedAt string `json:"deleted_at" gorm:"type:datetime;default:null"`
 }
