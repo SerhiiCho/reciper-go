7346e452df10ce0839c7450a530d365e12906000
SerhiiCho <serhiicho@protonmail.com>
2019-10-09 18:50:35 +0300

    Refactored api.go

--- backend/api/api.go
+++ backend/api/api.go
@@ -2,26 +2,36 @@ package api
 
 import (
 	"github.com/SerhiiCho/reciper/backend/app"
+	"github.com/SerhiiCho/reciper/backend/app/middleware"
 	"github.com/gin-gonic/gin"
-	"log"
 )
 
 // API struct
 type API struct {
-	App *app.App
+	App    *app.App
+	config *Config
+	router *gin.Engine
 }
 
 // NewAPI creates app struct
-func NewAPI(app *app.App) *API {
-	return &API{App: app}
+func NewAPI(app *app.App, config *Config) *API {
+	return &API{
+		App:    app,
+		config: config,
+		router: gin.Default(),
+	}
 }
 
-// Init initializes the api routes
-func (api *API) Init(router *gin.Engine) {
-	router.GET("/api/recipes", api.RecipeIndex())
-	router.POST("/api/recipes", api.RecipeCreate())
+// Start configures routes and start the server
+func (api *API) Start() error {
+	api.configureRouter()
+	api.router.Use(middleware.GetAll()...)
 
-	if err := router.Run(); err != nil {
-		log.Fatal("Can't serve the app")
-	}
+	return api.router.Run(api.config.BindAddr)
+}
+
+// configureRouter adds routes
+func (api *API) configureRouter() {
+	api.router.GET("/api/recipes", api.RecipeIndex())
+	api.router.POST("/api/recipes", api.RecipeCreate())
 }
