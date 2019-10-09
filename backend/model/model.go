0ba7cb6ff002c75bbe7c7aea29d9acecfa40fecc
SerhiiCho <serhiicho@protonmail.com>
2019-10-09 10:33:09 +0300

    Fixed models tags

--- backend/model/model.go
+++ backend/model/model.go
@@ -1,7 +1,7 @@
 package model
 
 type Model struct {
-	ID        uint   `json:"id" gorm:"type:int(10);unsigned;not null;AUTO_INCREMENT;primary_key" `
+	ID        uint   `json:"id" gorm:"type:int(10) unsigned auto_increment;primary_key"`
 	CreatedAt string `json:"created_at" gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
 	UpdatedAt string `json:"updated_at" gorm:"type:datetime;default:CURRENT_TIMESTAMP"`
 	DeletedAt string `json:"updated_at" gorm:"type:datetime;default:null"`
--- backend/model/recipe.go
+++ backend/model/recipe.go
@@ -5,22 +5,22 @@ type Recipe struct {
 	Model
 	User          User   `json:"-" gorm:"association_foreignkey:UserID"`
 	UserID        uint   `json:"-" gorm:"type:int(10);unsigned;not null"`
-	TitleRu       string `json:"title" gorm:"type:varchar(100);default:null"`
-	TitleEn       string `json:"title" gorm:"type:varchar(100);default:null"`
-	IntroRu       string `json:"intro" gorm:"type:varchar(210);default:null"`
-	IntroEn       string `json:"intro" gorm:"type:varchar(210);default:null"`
-	TextRu        string `json:"text" gorm:"type:text;default:null"`
-	TextEn        string `json:"text" gorm:"type:text;default:null"`
-	IngredientsRu string `json:"ingredients" gorm:"type:text;default:null"`
-	IngredientsEn string `json:"ingredients" gorm:"type:text;default:null"`
+	TitleRu       string `json:"title_ru" gorm:"type:varchar(100);default:null"`
+	TitleEn       string `json:"title_en" gorm:"type:varchar(100);default:null"`
+	IntroRu       string `json:"intro_ru" gorm:"type:varchar(210);default:null"`
+	IntroEn       string `json:"intro_en" gorm:"type:varchar(210);default:null"`
+	TextRu        string `json:"text_ru" gorm:"type:text;default:null"`
+	TextEn        string `json:"text_en" gorm:"type:text;default:null"`
+	IngredientsRu string `json:"ingredients_ru" gorm:"type:text;default:null"`
+	IngredientsEn string `json:"ingredients_en" gorm:"type:text;default:null"`
 	Slug          string `json:"slug" gorm:"type:varchar(100);not null"`
 	Time          uint64 `json:"time" gorm:"type:smallint(5);unsigned;default:'0'"`
 	Image         string `json:"image" gorm:"type:varchar(200);unsigned;default:'default.jpg'"`
-	ReadyRu       byte   `json:"ready" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
-	ReadyEn       byte   `json:"ready" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
-	ApprovedRu    byte   `json:"approved" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
-	ApprovedEn    byte   `json:"approved" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
-	PublishedRu   byte   `json:"published" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
-	PublishedEn   byte   `json:"published" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	ReadyRu       byte   `json:"ready_ru" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	ReadyEn       byte   `json:"ready_en" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	ApprovedRu    byte   `json:"approved_ru" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	ApprovedEn    byte   `json:"approved_en" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	PublishedRu   byte   `json:"published_ru" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
+	PublishedEn   byte   `json:"published_en" gorm:"type:tinyint(1);unsigned;default:'0';not null"`
 	Simple        byte   `json:"simple" form:"type:tinyint(1);unsigned;default:'0';not null"`
 }
