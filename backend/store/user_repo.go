2f713ad0ab63205923cd5ddd79455efd0bcd6f29
SerhiiCho <serhiicho@protonmail.com>
2019-10-10 18:00:12 +0300

    Added testing.go, store_test.go, user_repo_test.go and changed user_repo

--- backend/store/user_repo.go
+++ backend/store/user_repo.go
@@ -1,6 +1,8 @@
 package store
 
-import "github.com/SerhiiCho/reciper/backend/model"
+import (
+	"github.com/SerhiiCho/reciper/backend/model"
+)
 
 // UserRepo database repository
 type UserRepo struct {
@@ -9,7 +11,27 @@ type UserRepo struct {
 
 // Create creates new user in database
 func (repo *UserRepo) Create(user *model.User) (*model.User, error) {
-	return nil, nil
+	stmt, stmtErr := repo.store.db.Prepare("INSERT INTO users (email, password) VALUES (?, ?)")
+
+	if stmtErr != nil {
+		return nil, stmtErr
+	}
+
+	res, execErr := stmt.Exec(user.Email, user.Password)
+
+	if execErr != nil {
+		return nil, execErr
+	}
+
+	userID, resErr := res.LastInsertId()
+
+	if resErr != nil {
+		return nil, resErr
+	}
+
+	user.ID = uint(userID)
+
+	return user, nil
 }
 
 // FindByEmail returns user from database with given email
---
+++ backend/store/user_repo_test.go
@@ -0,0 +1,25 @@
+package store_test
+
+import (
+	"github.com/SerhiiCho/reciper/backend/model"
+	"github.com/SerhiiCho/reciper/backend/store"
+	"testing"
+)
+
+func TestUserRepo_Create(t *testing.T) {
+	st, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users", "recipes")
+
+	user, err := st.User().Create(&model.User{
+		Email:    "user@example.com",
+		Password: "111",
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if user == nil {
+		t.Error("Method Create must return user but nil returned")
+	}
+}
