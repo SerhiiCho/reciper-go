d61e4c2386e7a188c7d000a6ba48cda9cab3b251
SerhiiCho <serhiicho@protonmail.com>
2019-10-10 20:38:06 +0300

    Added test to user_repo_test.go

--- backend/store/user_repo_test.go
+++ backend/store/user_repo_test.go
@@ -7,21 +7,51 @@ import (
 )
 
 func TestUserRepo_Create(t *testing.T) {
-	t.Parallel()
-
 	st, teardown := store.TestStore(t, databaseURL)
 	defer teardown("users", "recipes")
 
-	user, err := st.User().Create(&model.User{
-		Email:    "user@example.com",
-		Password: "111",
-	})
+	email := "user@example.com"
+
+	user, err := st.User().Create(&model.User{Email: email})
 
 	if err != nil {
 		t.Error(err)
 	}
 
-	if user == nil {
-		t.Error("Method Create must return user but nil returned")
+	if user == nil || user.Email != email {
+		t.Errorf("Method Create must return user with email %s", email)
 	}
+}
+
+func TestUserRepo_FindByEmail(t *testing.T) {
+	st, teardown := store.TestStore(t, databaseURL)
+	defer teardown("users", "recipes")
+
+	email := "user@example.com"
+
+	t.Run("user doesn't exist in db", func(t *testing.T) {
+		_, err1 := st.User().FindByEmail(email)
+
+		if err1 == nil {
+			t.Error("FindByEmail must return error because user doesn't exist")
+		}
+	})
+
+	t.Run("user exist in db", func(t *testing.T) {
+		_, errUserCreate := st.User().Create(&model.User{Email: email})
+
+		if errUserCreate != nil {
+			t.Fatal("Can't create user in database", errUserCreate)
+		}
+
+		user, err2 := st.User().FindByEmail(email)
+
+		if err2 != nil {
+			t.Error("FindByEmail should not return error because user exist in db")
+		}
+
+		if user == nil || user.Email != email {
+			t.Errorf("FindByEmail must return user with email %s", email)
+		}
+	})
 }
