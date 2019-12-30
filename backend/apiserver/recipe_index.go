6b647f3c93cb3f45146732ee7bd91542c4cde556
SerhiiCho <serhiicho@protonmail.com>
2019-12-30 21:17:24 +0200

    Added code to recipeIndex method in recipe_index

--- backend/apiserver/recipe_index.go
+++ backend/apiserver/recipe_index.go
@@ -1,12 +1,22 @@
 package apiserver
 
 import (
+	"encoding/json"
+	"github.com/SerhiiCho/reciper/backend/utils"
 	"net/http"
 )
 
 // recipeIndex handles GET request on showing the list of all recipes
 func (serv *server) recipeIndex() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
+		recipes := []map[string]string{
+			map[string]string{
+				"title":   "Cool title",
+				"content": "Cool content",
+			},
+		}
+		result, _ := json.Marshal(recipes)
+		_, err := w.Write([]byte(result))
+		utils.HandleError("error trying to write response for recipes index", err, "")
 	}
 }
