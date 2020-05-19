944a528edadfb0859ee7760d877a8d82a2a40bbf
serhii <serhiicho@protonmail.com>
2020-05-19 19:30:26 +0300

    Changed Recipe struct field names

--- backend/model/recipe.go
+++ backend/model/recipe.go
@@ -5,32 +5,25 @@ import valid "github.com/go-ozzo/ozzo-validation"
 // Recipe model
 type Recipe struct {
 	Model
-	User          User   `json:"-"`
-	UserID        uint   `json:"-"`
-	TitleRu       string `json:"title_ru"`
-	TitleEn       string `json:"title_en"`
-	IntroRu       string `json:"intro_ru"`
-	IntroEn       string `json:"intro_en"`
-	TextRu        string `json:"text_ru"`
-	TextEn        string `json:"text_en"`
-	IngredientsRu string `json:"ingredients_ru"`
-	IngredientsEn string `json:"ingredients_en"`
-	Slug          string `json:"slug"`
-	Time          uint64 `json:"time"`
-	Image         string `json:"image"`
-	ReadyRu       byte   `json:"ready_ru"`
-	ReadyEn       byte   `json:"ready_en"`
-	ApprovedRu    byte   `json:"approved_ru"`
-	ApprovedEn    byte   `json:"approved_en"`
-	PublishedRu   byte   `json:"published_ru"`
-	PublishedEn   byte   `json:"published_en"`
-	Simple        byte   `json:"simple"`
+	User        User   `json:"-"`
+	UserID      uint   `json:"-"`
+	Title       string `json:"title"`
+	Intro       string `json:"intro"`
+	Text        string `json:"text"`
+	Ingredients string `json:"ingredients"`
+	Slug        string `json:"slug"`
+	Time        uint64 `json:"time"`
+	Image       string `json:"image"`
+	Ready       byte   `json:"ready"`
+	Approved    byte   `json:"approved"`
+	Published   byte   `json:"published"`
+	Simple      byte   `json:"simple"`
 }
 
 // Validate validates recipe data
 func (recipe *Recipe) Validate() error {
 	return valid.ValidateStruct(
 		recipe,
-		valid.Field(&recipe.TitleRu, valid.Required),
+		valid.Field(&recipe.Title, valid.Required),
 	)
 }
